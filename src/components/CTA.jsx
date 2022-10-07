dfd2c2d87cbb9511550761f396565c820c8549ab
unknown <german.af86@gmail.com>
2022-10-07 17:32:12 -0300

    cv es

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import CV from '../assets/cv/CV_linkedIn.pdf';
+import CVen from '../assets/cv/Resume.pdf';
+import CVes from '../assets/cv/CV_linkedIn.pdf';
 
 export const CTA = () => {
   return (
+    <div>
     <div className='cta'>
-      <a href={CV} download className='btn btn-primary'>Download my CV</a>
+      <a href={CVen} download className='btn btn-primary'>Download my CV</a>
     </div>
+    <br></br>
+    <div className='cta'>
+    <a href={CVes} download className='btn btn-primary'>CV en Español</a>
+  </div>
+  </div>
   )
 }
